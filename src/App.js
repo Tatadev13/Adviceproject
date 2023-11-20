df9fe2c1e4c23d14709ac8a6f0bf3101bd0f326d
Tata13 <marlene.legaspim@gmail.com>
2023-11-19 21:51:15 -0600

    Navbar

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import Navbar from './Navbar';
+import { Routes, Route, Link } from 'react-router-dom';
+
 
 
 
 function App() {
   return (
     <div className='App'>
+      <Routes>
+        <Route path= 'about' element={<About />} />
+        <Route path= 'contact' element={<Contact />} />
+     </Routes>
+
        <Navbar />
         
         <p>
@@ -22,6 +29,27 @@ function App() {
        <AdviceModal />
 
 
+    </div>
+  );
+}
+
+<hr />
+
+function About() {
+  return (
+    <div>
+      {/* <h3>You don't need to be alone to carry the burden.
+        We can absolutely help you.
+      </h3> */}
+    </div>
+  );
+}
+
+<hr />
+function Contact() {
+  return (
+    <div>
+      <h3>Contact us anytime</h3>
     </div>
   );
 }
